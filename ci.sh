1490b8ff889281cffb32c50e4285c4665d98c9fb
Adrian Kelly <adrianbkelly@gmail.com>
2016-09-10 16:06:20 +1000

    Fix ci bash script to upgrade based on grails major and minor versions

--- ci.sh
+++ ci.sh
@@ -3,14 +3,17 @@ set -e
 
 nVersion="${GRAILS_VERSION//.}";
 
-echo "Using a Grails version of $GRAILS_VERSION"
+array=(${GRAILS_VERSION//./ })
+major=${array[0]}
+minor=${array[1]}
+echo "Major:${major} Minor:${minor}"
 
+echo "Using a Grails version of $GRAILS_VERSION"
 source "/home/travis/.sdkman/bin/sdkman-init.sh"
 sdk use grails $GRAILS_VERSION
 
 grails clean
-
-if [ $nVersion -lt 240 ]
+if (( $major <= 2 && $minor < 4 ))
 then
   echo "Using 'grails upgrade' to upgrade application"
   grails upgrade --non-interactive
@@ -20,5 +23,4 @@ else
 fi
 
 grails test-app --unit
-
 grails package-plugin
