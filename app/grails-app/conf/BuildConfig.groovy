83655cb56b7d7e86f9094a599bb86b5acadb40d2
Adrian Kelly <adrianbkelly@gmail.com>
2016-09-10 21:02:59 +1000

    remove more deps

--- app/grails-app/conf/BuildConfig.groovy
+++ app/grails-app/conf/BuildConfig.groovy
@@ -46,19 +46,12 @@ grails.project.dependency.resolution = {
     plugins {
         runtime ":jquery:1.8.3"
         runtime ":resources:1.1.6"
-
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
         //runtime ":cached-resources:1.0"
         //runtime ":yui-minify-resources:0.1.5"
-
-        build ":tomcat:7.0.55.2" // or ":tomcat:8.0.20"
-        runtime ":database-migration:1.3.2"
-
         compile ':cache:1.0.1'
-
         compile ":partition-tests:0.2"
-
         test(":spock:0.7") {
             exclude "spock-grails-support"
         }
