483f03a75b0f3cb4ad3eb92726df00d4da987fa0
Adrian Kelly <adrianbkelly@gmail.com>
2016-09-10 19:53:41 +1000

    Use a functuion to upgrade grails

--- ci.sh
+++ ci.sh
@@ -12,34 +12,26 @@ echo "Using a Grails version of $GRAILS_VERSION"
 source "/home/travis/.sdkman/bin/sdkman-init.sh"
 sdk use grails $GRAILS_VERSION
 
+upgrade_grails(){
+    if (( $major <= 2 && $minor < 4 ))
+    then
+      echo "Using 'grails upgrade' to upgrade application"
+      grails upgrade --non-interactive
+    else
+      echo "Using 'set-version' to upgrade application"
+      grails set-version $GRAILS_VERSION
+      grails set-grails-version $GRAILS_VERSION
+    fi
+}
 
 cd "grails-partition-tests"
+upgrade_grails
 grails clean
-
-if (( $major <= 2 && $minor < 4 ))
-then
-  echo "Using 'grails upgrade' to upgrade application"
-  grails upgrade --non-interactive
-else
-  echo "Using 'set-version' to upgrade application"
-  grails set-version $GRAILS_VERSION
-  grails set-grails-version $GRAILS_VERSION
-fi
-
 grails test-app --unit
 grails package-plugin
 
-
 cd "../app"
-if (( $major <= 2 && $minor < 4 ))
-then
-  echo "Using 'grails upgrade' to upgrade application"
-  grails upgrade --non-interactive
-else
-  echo "Using 'set-version' to upgrade application"
-  grails set-version $GRAILS_VERSION
-  grails set-grails-version $GRAILS_VERSION
-fi
-
+upgrade_grails
+grails clean
 grails test partition-test \"--split=1\" \"--totalSplits=2\"
 grails test partition-test \"--split=12\" \"--totalSplits=2\"
